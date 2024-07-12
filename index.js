812e6a7480d761133a090c420eafa79a47e68c79
daffaYuharshu <daffayuharshu@gmail.com>
2024-07-12 23:13:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.10/swagger-ui.min.css";
+
+
+const apiDocumentation = require("./creditscoring.json");
+const port = 3001;
+
+const app = express();
+
+app.use("/", swaggerUI.serve, swaggerUI.setup(apiDocumentation, {customCssUrl: CSS_URL}));
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
